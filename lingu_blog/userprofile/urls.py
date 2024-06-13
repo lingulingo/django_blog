c0ff7e3ab14de892cd8fa0a6a28d75ae004551d9
lingulingo <andrewfu@soarinfotech.com>
2024-06-13 14:13:46 +0800

    博客_用户模块

--- lingu_blog/userprofile/urls.py
+++ lingu_blog/userprofile/urls.py
@@ -6,4 +6,7 @@ app_name = 'userprofile'
 urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
+    path('register/', views.user_register, name='register'),
+    # 用户删除
+    path('delete/<int:id>/', views.user_delete, name='delete'),
 ]
--- lingu_blog/userprofile/views.py
+++ lingu_blog/userprofile/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
-from .forms import UserLoginForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .forms import UserLoginForm, UserRegisterForm
 
 
 # Create your views here.
@@ -33,3 +35,69 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return redirect("article:article_list")
+
+
+def user_register(request):
+    if request.method == "POST":
+        user_register_form = UserRegisterForm(request.POST)
+        if user_register_form.is_valid():
+            new_user = user_register_form.save(commit=False)
+            # 设置密码
+            new_user.set_password(user_register_form.cleaned_data['password'])
+            new_user.save()
+            # 保存好数据后立即登录并返回博客列表页面
+            login(request, new_user)
+            return redirect("article:article_list")
+        else:
+            return HttpResponse("注册表单输入有误。请重新输入~")
+    elif request.method == 'GET':
+        user_register_form = UserRegisterForm()
+        context = {'form': user_register_form}
+        return render(request, 'userprofile/register.html', context)
+    else:
+        return HttpResponse("请使用GET或POST请求数据")
+
+
+@login_required(login_url='/userprofile/login/')
+def user_delete(request, id):
+    if request.method == "POST":
+        user = User.objects.get(id=id)
+        # 验证登录用户、待删除用户是否相同
+        if request.user == user:
+            # 退出登录，删除数据并返回博客列表
+            logout(request)
+            user.delete()
+            return redirect("article:article_list")
+        else:
+            return HttpResponse("你没有删除操作的权限。")
+    else:
+        return HttpResponse("仅接受post请求。")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
